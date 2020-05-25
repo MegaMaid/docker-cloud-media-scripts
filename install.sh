3a9baeec97da4fe66075d287995cefd659089d0c
Lee Hilton <lee.hilton@leezilla.net>
2020-05-25 14:36:36 -0700

    Updated rclone to latest,

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Rclone variables
-_rclone_version="v1.42"
+_rclone_version="v1.51.0"
 rclone_release="rclone-${_rclone_version}-linux-amd64"
 rclone_zip="${rclone_release}.zip"
 rclone_url="https://github.com/ncw/rclone/releases/download/${_rclone_version}/${rclone_zip}"
 # Plexdrive variables
-plexdrive_bin="plexdrive-linux-amd64"
-plexdrive_url="https://github.com/dweidenfeld/plexdrive/releases/download/5.0.0/${plexdrive_bin}"
+# plexdrive_bin="plexdrive-linux-amd64"
+# plexdrive_url="https://github.com/dweidenfeld/plexdrive/releases/download/5.0.0/${plexdrive_bin}"
 # Rclone
 wget "$rclone_url"
 unzip "$rclone_zip"
@@ -15,7 +15,7 @@ cp -rf "${rclone_release}/rclone" "/usr/bin/rclone"
 rm -rf "$rclone_zip"
 rm -rf "$rclone_release"
 # Plexdrive
-wget "$plexdrive_url"
-chmod a+x "$plexdrive_bin"
-cp -rf "$plexdrive_bin" "/usr/bin/plexdrive"
-rm -rf "$plexdrive_bin"
+# wget "$plexdrive_url"
+# chmod a+x "$plexdrive_bin"
+# cp -rf "$plexdrive_bin" "/usr/bin/plexdrive"
+# rm -rf "$plexdrive_bin"
